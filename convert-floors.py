07a7a15b9a03b9bd35715818c9aac2a8a222cf2b
Achraf333-m <achrafdaimallah2003@gmail.com>
2023-09-06 13:39:55 -0400

    intro python

---
+++ convert-floors.py
@@ -0,0 +1,6 @@
+#take european floor number and convert it to its equivalent in the USA.
+name = input("what's your name:")
+print("hello", name)
+
+inp = input("European floor number:")
+usFloor = print(int(inp) + 1)
